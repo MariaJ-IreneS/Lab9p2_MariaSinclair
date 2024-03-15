eca2efdbc43a1eca0d1ca1af7a59afd2cbbb7923
MariaJ-IreneS <misinclair15@unitec.edu>
2024-03-15 13:28:45 -0600

    3push

---
+++ src/Lab9/Fecha_Hora.java
@@ -0,0 +1,18 @@
+package Lab9;
+
+import javax.swing.JLabel;
+
+public class Fecha_Hora implements Runnable {
+
+    private JLabel hora, fecha;
+
+    public Fecha_Hora(JLabel hora, JLabel fecha) {
+        this.hora = hora;
+        this.fecha = fecha;
+    }
+
+    public void run() {
+
+    }
+
+}
